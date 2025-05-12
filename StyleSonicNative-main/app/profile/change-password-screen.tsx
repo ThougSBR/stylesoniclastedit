89fe6bb882157bd950fdbb194262c1659ab787ab
ThougSBR <tkahnss@gmail.com>
2025-05-12 17:10:53 +0300

    Update change-password-screen.tsx

--- StyleSonicNative-main/app/profile/change-password-screen.tsx
+++ StyleSonicNative-main/app/profile/change-password-screen.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import ChangePassword from "../../components/changepassword";
+import React from "react"; // Importing React to use JSX and React components
+import ChangePassword from "../../components/changepassword"; // Importing the ChangePassword component
 
+// ChangePasswordScreen component: This is the screen that will display the change password form
 const ChangePasswordScreen: React.FC = () => {
-  return <ChangePassword />;
+  return <ChangePassword />; // Rendering the ChangePassword component
 };
 
-export default ChangePasswordScreen;
+export default ChangePasswordScreen; // Exporting the component for use in other parts of the app
